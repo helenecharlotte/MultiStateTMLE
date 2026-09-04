75d5b78e62b48859bc8a477d1e0b672b78c7c562
helenecharlotte <helenerytgaard@gmail.com>
2026-09-04 13:59:23 +0200

    update master.R

--- usage/master.R
+++ usage/master.R
@@ -3,9 +3,9 @@
 ## Author: Helene
 ## Created: Feb  5 2026 (10:09) 
 ## Version: 
-## Last-Updated: May 13 2026 (20:04) 
+## Last-Updated: Sep  4 2026 (13:58) 
 ##           By: Helene
-##     Update #: 17
+##     Update #: 24
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -31,10 +31,14 @@ run.master <- function(install.simevent = FALSE, file.path = "") {
     if (install.simevent) remotes::install_github("miclukacova/simevent")
     library(simevent)
 
+    source(paste0("./", file.path, "R/sim.from.data.R"))
+    source(paste0("./", file.path, "R/sim.generic.R"))
     source(paste0("./", file.path, "R/prepare.initial.R"))
     source(paste0("./", file.path, "R/compute.Q.clever.per.id.R"))
     source(paste0("./", file.path, "R/tmle.alpha.fun.R"))
-    source(paste0("./", file.path, "R/calibration.fun.R"))    
+    source(paste0("./", file.path, "R/calibration.curve.fun.R"))    
+    source(paste0("./", file.path, "R/calibration.fun.R"))
+    source(paste0("./", file.path, "R/make.calibrated.contrasts.R"))        
     source(paste0("./", file.path, "R/estimate.alpha.fun.R"))
     source(paste0("./", file.path, "R/estimate.derivative.R"))
 }
